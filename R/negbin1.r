6622a61e0561a361af3a0da86f8247893c123dda
Mark W. Donoghoe <markdonoghoe@gmail.com>
2017-12-23 22:22:06 +0000

    version 3.0

--- R/negbin1.r
+++ R/negbin1.r
@@ -3,17 +3,21 @@ negbin1 <- function (link, phi = stop("'phi' must be given"))
 	.Phi <- phi
 	env <- new.env(parent = .GlobalEnv)
 	assign(".Phi", phi, envir = env)
-	stats <- make.link("identity")
+	lnk <- make.link("identity")
 	variance <- function(mu) mu * (1 + .Phi)
-  valideta <- function(eta) all(eta >= 0)
 	validmu <- function(mu) all(mu >= 0)
 	dev.resids <- function(y, mu, wt) {
-		r <- (mu - y)/.Phi * log(1 + .Phi)
+	  y.un <- unique(y)
+	  mu.un <- rep(NA, length(y.un))
+	  mu.un[y.un == 0] <- 0
+	  mu.un[y.un == 1] <- .Phi / log(1 + .Phi)
+	  rootfn <- function(mu, y, phi) digamma(y + mu/phi) - digamma(mu/phi) - log(1 + phi)
+	  getroot <- function(y, phi) uniroot(rootfn, c(ifelse(phi <= 1, phi*y, y), phi*y/log(1+phi)), y = y, phi = phi)$root
+	  mu.un[y.un > 1] <- sapply(y.un[y.un > 1], getroot, phi = .Phi)
+	  mu.s <- mu.un[match(y, y.un)]
+		r <- (mu - mu.s)/.Phi * log(1 + .Phi)
 		p <- which(y > 0)
-		r[p] <- r[p] + (mapply(function(Y,m,phi) {
-								k <- seq_len(Y) - 1
-								sum(log((Y/phi + k) / (m/phi + k)))},
-							y[p], mu[p], MoreArgs = list(phi = .Phi)))
+		r[p] <- r[p] + lgamma(y[p] + mu.s[p]/.Phi) - lgamma(mu.s[p]/.Phi) - lgamma(y[p] + mu[p]/.Phi) + lgamma(mu[p]/.Phi)
 		2 * r
 	}
 	aic <- function(y, n, mu, wt, dev) -2 * sum(dnbinom(y, size = mu/.Phi, prob = 1/(1+.Phi), log = TRUE))
@@ -29,9 +33,9 @@ negbin1 <- function (link, phi = stop("'phi' must be given"))
 	}
     fname <- paste("negbin1 (phi = ", format(round(phi, 4)), ")", sep = "")
 	environment(variance) <- environment(validmu) <- environment(dev.resids) <- environment(aic) <- environment(simfun) <- env
-	structure(list(family = fname, link = "identity", linkfun = stats$linkfun,
-		linkinv = stats$linkinv, variance = variance, dev.resids = dev.resids,
-		aic = aic, mu.eta = stats$mu.eta, initialize = initialize,
-		valid.mu = validmu, valid.eta = valideta, simulate = simfun),
+	structure(list(family = fname, link = "identity", linkfun = lnk$linkfun,
+		linkinv = lnk$linkinv, variance = variance, dev.resids = dev.resids,
+		aic = aic, mu.eta = lnk$mu.eta, initialize = initialize,
+		valid.mu = validmu, valid.eta = lnk$valideta, simulate = simfun),
 		class = "family")
 }
